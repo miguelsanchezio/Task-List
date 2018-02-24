0a2ed1a15038857576a663e5c2b34bc1b08ff58e
Miguel Sanchez <msanchez.anaheim@gmail.com>
2018-02-24 12:16:33 -0800

    Added the ability to remove a task, remove all tasks, and filter tasks

--- app.js
+++ app.js
@@ -11,6 +11,12 @@ loadEventListeners();
 function loadEventListeners() {
     // Add task event
     form.addEventListener('submit', addTask);
+    // Remove task event
+    taskList.addEventListener('click', removeTask);
+    // Clear tasks event
+    clearBtn.addEventListener('click', clearTasks);
+    // Filter tasks event
+    filter.addEventListener('keyup', filterTasks);
 }
 
 // Add task
@@ -40,4 +46,37 @@ function addTask(e) {
     }
 
     e.preventDefault();
+}
+
+// Remove task
+function removeTask(e) {
+    if(e.target.parentElement.classList.contains('delete-item')) {
+        if(confirm('Are you sure?')) {
+            e.target.parentElement.parentElement.remove();
+        }
+    }
+}
+
+// Clear tasks
+function clearTasks() {
+    // taskList.innerHTML = '';
+
+    while(taskList.firstChild) {
+        taskList.removeChild(taskList.firstChild);
+    }
+}
+
+// Filter Tasks
+function filterTasks(e) {
+    const text = e.target.value.toLowerCase();
+
+    document.querySelectorAll('.collection-item').forEach(function(task) {
+        const item = task.firstChild.textContent;
+
+        if(item.toLowerCase().indexOf(text) != -1) {
+            task.style.display = 'block';
+        } else {
+            task.style.display = 'none';
+        }
+    });
 }
